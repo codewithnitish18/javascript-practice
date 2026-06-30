83b2274db0153239d3a37361340435046a89663e
codewithnitish18 <ethereal.nitish05@gmail.com>
2026-06-30 08:29:10 +0530

    singelton object and about how to access the values

---
+++ 02_Basics/04_object.js
@@ -0,0 +1,37 @@
+//singleton object
+
+// const tinderUser = new Object()
+// console.log(tinderUser);
+
+//non singelton object
+const tinderUser = {}
+tinderUser.id = "123bc"
+tinderUser.name = "jack"
+tinderUser.isLoggedIn = false
+// console.log(tinderUser);
+
+ const regularUser = {
+    email : "ritviknitish@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Virat",
+            lastname: "kohli"
+        }
+    }
+ }
+
+ console.log(regularUser.fullname.userfullname.firstname);
+
+
+ const obj1 = {1: "a" , 2: "b"}
+ const obj2 = {3: "a" , 4: "b"}
+//  const obj3 = Object.assign({}, obj1 , obj2)
+
+ const obj3 = {...obj1, ...obj2}
+ console.log(obj3);
+ 
+ //aasign is used to copy all enumerable own properties from source code to target code
+ 
+ console.log(Object.keys(tinderUser));
+ console.log(Object.values(tinderUser));
+ 
--- 02_Basics/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-marvel_heroes.push(dc_Heroes)
-// console.log(marvel_heroes);
-
-// console.log(marvel_heroes[3][1]);
