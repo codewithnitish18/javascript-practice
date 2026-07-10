9bc3d516c10201aad691f1ae048ed993164ee9d5
codewithnitish18 <ethereal.nitish05@gmail.com>
2026-07-10 20:38:58 +0530

    looping statements

---
+++ CompeteWithMe/looping.js
@@ -0,0 +1,23 @@
+//looping means to repeat a same block of code untill the conditions becomes false
+
+//FOR LOOP STATEMENT
+//IT IS USED TO REPEAT a block if code for a specific number of times
+
+for(let i = 0; i<=5; i++){
+    console.log(i);
+}
+
+//for(intialization;condition;increment/decrement){
+//code block to be executed
+//}
+
+//break stop the entire loop and exit the loop
+
+
+
+for (let run = 0; run<=40;run++){
+    console.log(run);
+    if(run==18){
+        break;
+    }
+}
