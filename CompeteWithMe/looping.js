dd976c8afcae3b46463274d5bc49194445fd1601
codewithnitish18 <ethereal.nitish05@gmail.com>
2026-07-13 17:50:30 +0530

    today i studied the remaining loop statement

--- CompeteWithMe/looping.js
+++ CompeteWithMe/looping.js
@@ -21,3 +21,36 @@ for (let run = 0; run<=40;run++){
         break;
     }
 }
+
+
+//continue statement is used to skip the curent iteration of the loop and continue with the next iteration
+
+
+for (let devlopment = 0;devlopment<=40;devlopment++){
+    console.log(devlopment);
+    if(devlopment == 35){
+        continue;
+  
+    }
+}
+
+
+//WHILE LOOP STATEMENT is used when the number of iteration is unknown...
+ let i = 10;
+ while (i>=1){
+    console.log(i);
+    i--;
+ }
+
+
+ //DO WHILE LOOP STATEMENT is used to execute the code first and check the condition after executing the code block. it is used when we want to execute the code at least once.
+
+
+ let n = 1;
+
+ do{
+    console.log(n);
+    n++;
+ }
+while (n<=5);
+ 
---
+++ CompeteWithMe/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+
+
+for (let run = 0; run<=40;run++){
+    console.log(run);
+    if(run==18){
+        break;
+    }
+}
+
+
+//continue statement is used to skip the curent iteration of the loop and continue with the next iteration
+
+
+for (let devlopment = 0;devlopment<=40;devlopment++){
+    console.log(devlopment);
+    if(devlopment===35){
