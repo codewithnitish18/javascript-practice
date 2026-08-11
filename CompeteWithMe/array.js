a5a15bbf6283bd9b6a2ae021c670c04d24a23fb9
codewithnitish18 <ethereal.nitish05@gmail.com>
2026-08-11 17:05:58 +0530

    In this i am learning the array methods and how to implement them

---
+++ CompeteWithMe/array.js
@@ -0,0 +1,25 @@
+// let players = ["Rohit", "Dhoni", "AB"];
+//  players.unshift("Virat");
+//  players.shift();
+//  console.log(players);
+
+
+ //INCLUDES
+ //it is used to find the find the element in a array
+ //so basically include is usedto check whether the elemnent ias present or exist in the array
+
+
+//   let files = ["resume.pdf", "photo.jpg", "notes.pdf"];
+//   console.log(files.includes("resume.pdf"));
+
+//   let players = ["Virat","Rohit","Dhoni","AB"];
+//   console.log(players.includes("Virat"));
+//   console.log(players.includes("Sachin"));
+
+//INDEXOF
+//sometimes we dont want that something exist or not we also want that where it is exist and and where it is so it iis used to tell whher it is
+
+let files = ["resume.pdf", "photo.jpg", "notes.pdf", "project.zip"];
+console.log(files.indexOf("notes.pdf"));
+console.log(files.indexOf("abc.pdf"));
+console.log(files.includes("project.zip"));
